352eac4659077ba57c5a4a8d21293b2af33b3848
Ken-Lauren Daganio <akosiken@gmail.com>
2018-03-23 16:59:15 +0800

    adds tmuxinator confs

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/gvimrc ~/.gvimrc
 ln -s `pwd`/vim ~/.vim
 ln -s `pwd`/tmux.conf ~/.tmux.conf
+ln -s `pwd`/tmuxinator ~/.tmuxinator
 ln -s `pwd`/gitconfig ~/.gitconfig
 
 echo "All done!"

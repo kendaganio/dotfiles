e803126f68a08d1e54c17c556f398d6c65c21d22
Ken-Lauren Daganio <akosiken@gmail.com>
2018-04-19 12:24:09 +0800

    more tmuxinator, ranger confs

--- install.sh
+++ install.sh
@@ -2,11 +2,12 @@
 
 echo "Let's start this mofo up!"
 
-ln -s `pwd`/vimrc ~/.vimrc
-ln -s `pwd`/gvimrc ~/.gvimrc
-ln -s `pwd`/vim ~/.vim
-ln -s `pwd`/tmux.conf ~/.tmux.conf
-ln -s `pwd`/tmuxinator ~/.tmuxinator
-ln -s `pwd`/gitconfig ~/.gitconfig
+ln -sv `pwd`/vimrc ~/.vimrc
+ln -sv `pwd`/gvimrc ~/.gvimrc
+ln -sv `pwd`/vim ~/.vim
+ln -sv `pwd`/tmux.conf ~/.tmux.conf
+ln -sv `pwd`/tmuxinator ~/.tmuxinator
+ln -sv `pwd`/gitconfig ~/.gitconfig
+ln -sv `pwd`/config/ranger ~/.config
 
 echo "All done!"
